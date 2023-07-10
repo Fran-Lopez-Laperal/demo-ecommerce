429904c60d2ad7d0e3af18b180dcdc025c3bbc7a
Francisco José López Laperal <fransanxenxo@gmail.com>
2023-07-10 19:18:49 +0200

    add home page

--- WEB/src/App.jsx
+++ WEB/src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import { Route, Routes } from "react-router-dom"
 import Register from './components/Register/Register'
 import Login from './components/Login/Login'
+import Home from './components/pages/Home/Home'
 
 function App() {
 
   return (
     <>
       <Routes>
+        <Route path='/' element={<Home />} />
         <Route path='register' element={<Register />} />
         <Route path='login' element={<Login />} />
       </Routes>
